e29869eb4ac9c38fca406089139877395b30b482
Haley May <haymon24u@gmail.com>
2022-11-10 13:56:11 -1000

    adds search and post pages in handlebar and adds routes for each

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,28 +1,32 @@
-const router = require('express').Router()
-const {
-    User, Post
-} = require('../models/')
+const router = require("express").Router();
+const { User, Post } = require("../models/");
 
-router.get('/',(req,res)=>{
-res.render('home',{
-    layout:'main'
-})
-})
+router.get("/", (req, res) => {
+  res.render("home", {
+    layout: "main",
+  });
+});
 
-router.get('/signup',(req,res)=>{
-    if (req.session.loggedIn){
-        res.redirect('/')
-        return
-    }
-    res.render('signup',{
-        layout:"main"
-    })
-})
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup", {
+    layout: "main",
+  });
+});
 
+router.get("/post", (req, res) => {
+  res.render("post", {
+    layout: "main",
+  });
+});
 
+router.get("/search", (req, res) => {
+  res.render("search", {
+    layout: "main",
+  });
+});
 
-
-
-
-
-module.exports = router
+module.exports = router;
