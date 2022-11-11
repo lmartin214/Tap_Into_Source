afa777a32f0bc9ad42ff8abd541d0d97de25f916
Jacqueline Cashman <jacquelinehcashman@gmail.com>
2022-11-11 00:27:49 -1000

    progress with test routes in js

--- models/Post.js
+++ models/Post.js
@@ -47,11 +47,17 @@ Post.init(
       allowNull: false,
       comments: "",
     },
-    tested: {
+    tests: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
       comments: "",
     },
+    // if yes, which tests have you done? what were the results? 10) does not apply instead of boolean
+    // testsDone: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    //   comment: "",
+    // },
 
     // distance: {
     //   type: DataTypes.STRING,
