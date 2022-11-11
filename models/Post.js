48bb64bbaa388ed35c9015b003ccbf5b7e0b067d
Jacqueline Cashman <jacquelinehcashman@gmail.com>
2022-11-10 16:34:53 -1000

    added filters for db table

--- models/Post.js
+++ models/Post.js
@@ -6,7 +6,7 @@ const sequelize = require("../config/connection");
 // Initialize post model (table) by extending off Sequelize's Model class
 class Post extends Model {}
 
-// set up fields and rules for Product model
+// set up fields and rules for Post model
 Post.init(
   {
     // define columns
@@ -19,6 +19,62 @@ Post.init(
     type: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    distance: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    tested: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    // isMoving: {
+    //   type: DataTypes.BOOLEAN,
+    //   allowNull: false,
+    // },
+    // isStill: {
+    //   type: DataTypes.BOOLEAN,
+    //   allowNull: false,
+    // },
+    source: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    drinkable: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    visibility: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    // depth: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    // },
+    swimmable: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    accessibility: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isHike: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    isDrive: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    parking: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    dogs: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
   },
   {
