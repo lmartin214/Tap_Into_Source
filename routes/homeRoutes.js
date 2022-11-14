698b0d8fd9cd4e68146c76147fd282df31c00608
Chase Binkley <chasebinkley@Chases-MacBook-Pro.local>
2022-11-13 16:28:55 -1000

    added profile handlebars and css

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -15,7 +15,7 @@ router.get('/signup',(req,res)=>{
         return
     }
     res.render('signup',{
-        layout:"signuplayout",
+        layout:"signup",
         style: "signup.css",
         
     })
