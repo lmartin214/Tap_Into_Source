16cbdb0c01040eed12c76ce9ff3bab7b7f6bb271
Haley May <haymon24u@gmail.com>
2022-11-14 23:35:08 -1000

    fixing issues with login html

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -11,33 +11,32 @@ router.get("/", (req, res) => {
 
 // router.get('/profile', withAuth ,(req,res)=>{
 
-router.get('/profile',(req,res)=>{
-    res.render('profile',{
-        layout:'profilelayout',
-        style: "profile.css",
-    })
-    })
-
-router.get('/signup',(req,res)=>{
-    if (req.session.loggedIn){
-        res.redirect('/')
-        return
-    }
-    res.render('signup',{
-        layout:"signuplayout",
-        style: "signup.css",
-        
-    })
-})
+router.get("/profile", (req, res) => {
+  res.render("profile", {
+    layout: "profilelayout",
+    style: "profile.css",
+  });
+});
 
 router.get("/signup", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
-    return
+    return;
   }
   res.render("signup", {
     layout: "signuplayout",
     style: "signup.css",
+  });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login", {
+    layout: "loginlayout",
+    style: "login.css",
   });
 });
 
@@ -47,8 +46,11 @@ router.get("/post", (req, res) => {
   });
 });
 
+router.get("/search", (req, res) => {
+  res.render("search", {
+    layout: "searchlayout",
+    style: "search.css",
+  });
+});
 
-
-
-
-module.exports = router
+module.exports = router;
