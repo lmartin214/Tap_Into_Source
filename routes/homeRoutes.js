e142c18dfa49c906f95564dd90dd55b6dcb61409
Haley May <haymon24u@gmail.com>
2022-11-14 15:14:23 -1000

    merges chases search html and css with mine

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,32 +1,29 @@
-const router = require('express').Router()
-const {
-    User, Post
-} = require('../models/')
-const withAuth = require('../utils/auth')
+const router = require("express").Router();
+const { User, Post } = require("../models/");
+const withAuth = require("../utils/auth");
 
-router.get('/',(req,res)=>{
-res.render('home',{
-    layout:'main'
-})
-})
+router.get("/", (req, res) => {
+  res.render("home", {
+    layout: "main",
+  });
+});
 
-router.get('/profile', withAuth ,(req,res)=>{
-    res.render('profile',{
-        layout:'main'
-    })
-    })
+router.get("/profile", withAuth, (req, res) => {
+  res.render("profile", {
+    layout: "main",
+  });
+});
 
-router.get('/signup',(req,res)=>{
-    if (req.session.loggedIn){
-        res.redirect('/')
-        return
-    }
-    res.render('signup',{
-        layout:"signup",
-        style: "signup.css",
-        
-    })
-})
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup", {
+    layout: "signuplayout",
+    style: "signup.css",
+  });
+});
 
 router.get("/post", (req, res) => {
   res.render("post", {
@@ -36,7 +33,8 @@ router.get("/post", (req, res) => {
 
 router.get("/search", (req, res) => {
   res.render("search", {
-    layout: "main",
+    layout: "searchlayout",
+    style: "search.css",
   });
 });
 
