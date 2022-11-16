2ab3ef80ccb9f2a3e37c2ac4facafef5443cb416
Haley May <haymon24u@gmail.com>
2022-11-16 06:51:04 -1000

    modified all seed files for correct dummy data

---
+++ seeds/user-seeds.js
@@ -0,0 +1,52 @@
+const { User } = require("../models");
+
+const userData = [
+  {
+    username: "tweedledee",
+    password: "password1234",
+  },
+  {
+    username: "haleymay",
+    password: "000000Zzz!!",
+  },
+  {
+    username: "Punksterrr789",
+    password: "Hiholetsgo333",
+  },
+  {
+    username: "shaquille.oatmeal",
+    password: "Bsktb0lll!",
+  },
+  {
+    username: "BenAfleckIsAnOkActor",
+    password: "YoYoYo666",
+  },
+  {
+    username: "BeFkn4Real",
+    password: "lmaoWhyyy",
+  },
+  {
+    username: "Its_6_AM",
+    password: "&IhaventSlept",
+  },
+  {
+    username: "Ham_Sandwich",
+    password: "Dijonayyyyyze123",
+  },
+  {
+    username: "Mowgli_tha_Prince",
+    password: "catzRule666",
+  },
+  {
+    username: "whytfamistilldoingthis",
+    password: "Goodnight!!!",
+  },
+  {
+    username: "IheartWater",
+    password: "lovemesumwatersourcesBBY",
+  },
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
