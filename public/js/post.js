201eba7d4d2239cf03327827a59cafb9842cf07b
JacquelineHockin <jacquelinehcashman@gmail.com>
2022-11-16 04:40:13 -1000

    merged and edited api google maps code from project partner, remains unworking

--- public/js/post.js
+++ public/js/post.js
@@ -41,3 +41,28 @@ const postForm = async function (event) {
 };
 
 document.querySelector("#post-form").addEventListener("submit", postForm);
+
+// API
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const hawaii = { lat: 19.741755, lng: 151.844437 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: hawaii,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: hawaii,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
+
+<script
+  async
+  defer
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_YP8aCoVnnRWfhHT6ozNbDEFPhshZ81s&callback=initMap"
+></script>;
