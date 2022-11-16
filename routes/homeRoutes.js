c86fd5540d0c7f3e38ca9d2cb369026ba8e80f88
JacquelineHockin <jacquelinehcashman@gmail.com>
2022-11-15 17:06:20 -1000

    beginning to format layout of page

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,8 +1,6 @@
-const router = require('express').Router()
-const {
-    User, Post
-} = require('../models/')
-const withAuth = require('../utils/auth')
+const router = require("express").Router();
+const { User, Post } = require("../models/");
+const withAuth = require("../utils/auth");
 
 router.get("/", (req, res) => {
   res.render("home", {
@@ -10,39 +8,38 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get('/profile', withAuth ,(req,res)=>{
-    res.render('profile',{
-        layout:'main'
-    })
-    })
+router.get("/profile", withAuth, (req, res) => {
+  res.render("profile", {
+    layout: "main",
+  });
+});
 
-router.get('/signup',(req,res)=>{
-    if (req.session.loggedIn){
-        res.redirect('/')
-        return
-    }
-    res.render('signup',{
-        layout:"signuplayout",
-        style: "signup.css",
-        
-    })
-})
-
-router.get('/login',(req,res)=>{
-  if (req.session.loggedIn){
-      res.redirect('/')
-      return
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
   }
-  res.render('login',{
-      layout:"loginlayout",
-      style: "login.css",
-      
-  })
-})
+  res.render("signup", {
+    layout: "signuplayout",
+    style: "signup.css",
+  });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login", {
+    layout: "loginlayout",
+    style: "login.css",
+  });
+});
 
 router.get("/post", (req, res) => {
   res.render("post", {
-    layout: "main",
+    layout: "postlayout",
+    style: "post.css",
   });
 });
 
