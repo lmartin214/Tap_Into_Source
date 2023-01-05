4360c34c973412360c180cee469985cfe4031a8f
lmartin214@yahoo.com <you@example.com>
2023-01-05 14:39:53 -0800

    fixed home page routes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -9,26 +9,20 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/profile", withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] }
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('profile', {
-      layout: "profilelayout",
-      style: "profile.css",
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
   }
+  res.render('login', {
+    logged_in: true,
+    layout: "loginlayout",
+    style: "login.css",
+  });
 });
 
 router.get("/signup", (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     res.redirect("/");
     return;
   }
@@ -38,16 +32,26 @@ router.get("/signup", (req, res) => {
   });
 });
 
-router.get("/login", (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect("/");
-    return;
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      //include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true,
+      layout: "profilelayout",
+      style: "profile.css",
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
-  res.render("login", {
-    layout: "loginlayout",
-    style: "login.css",
-  });
 });
+
 
 router.get("/post", (req, res) => {
   res.render("post", {
