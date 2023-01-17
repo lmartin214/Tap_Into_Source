7bf3987021a744092e510b42e1ea4b87e1ee473a
lmartin214@yahoo.com <you@example.com>
2023-01-17 15:13:10 -0800

    fixed withAuth helper, fixed session data, fixed login route + added logout route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,20 +1,22 @@
 const router = require("express").Router();
 const { User, Post } = require("../../models");
-const withAuth = require('../../utils/auth')
+const withAuth = require("../../utils/auth");
 // The `/api/users` endpoint
 
 router.post("/", async (req, res) => {
   try {
+    console.log("Singup pinged")
     const newUser = await User.create({
       username: req.body.username,
       password: req.body.password,
     });
+    console.log(newUser)
     req.session.save(() => {
-      (req.session.userId = newUser.id),
-        (req.session.username = newUser.username),
-        (req.session.loggedIn = true);
+    req.session.userId = newUser.id,
+        req.session.username = newUser.username,
+        req.session.logged_in = true;
+        res.json(newUser);
     });
-    res.json(newUser);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -24,28 +26,50 @@ router.put("/:id", withAuth, async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, async (req, res) => {
   // delete a category by its `id` value
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+  console.log("Hitting login route");
+  console.log(req.body);
   User.findOne({
-      where: {
-      email: req.body.username
-    }
-
-  }).then(dbUserData => {
-    if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email name!' });
-      return;
-    }
-
-    const validPassword = dbUserData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
-    }
+    where: {
+      username: req.body.username,
+    },
   })
-}),      
+    .then((dbUserData) => {
+      console.log(dbUserData);
+      if (!dbUserData) {
+        res.status(400).json({ message: "No user with that name!" });
+        return;
+      }
+
+      const validPassword = dbUserData.checkPassword(req.body.password);
+
+      if (!validPassword) {
+        res.status(400).json({ message: "Incorrect password!" });
+      }
+
+      req.session.save(() => {
+        req.session.userId = dbUserData.id,
+          req.session.username = dbUserData.username,
+          req.session.logged_in = true;
+          res.json(dbUserData)
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
+// Logout
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
