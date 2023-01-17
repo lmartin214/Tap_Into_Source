cdf5234f5241e4a9f666d22027da2e161768e7fb
lmartin214@yahoo.com <you@example.com>
2023-01-17 15:06:17 -0800

    fixed event listener to submit button

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,10 @@
 const loginBtn = async function(event){
     event.preventDefault();
-
+    console.log(event)
     const username = document.querySelector("#username-input-login")
     const password = document.querySelector("#password-input-login")
 
-    const response = await fetch("/api/user", {
+    const response = await fetch("/api/user/login", {
         method: "POST",
         body: JSON.stringify({
             username: username.value,
@@ -14,13 +14,16 @@ const loginBtn = async function(event){
             "Content-Type": "application/json"
         }
      });
-
+    console.log(response)
+    var data = await response.json()
+    console.log(data)
      if (response.ok) {
         console.log("profile")
         // redirect to whichever page you want to after you sign up //
+        document.location.replace('/profile');
      }
      else {
         alert("failed to sign up")
      }
  }
-document.querySelector('#login-form').addEventListener("login", loginBtn)
+document.querySelector('#login-form').addEventListener("submit", loginBtn)
